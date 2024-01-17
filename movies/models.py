bbb46293a1420471a78b35ea404a794ad3a9d7dd
Felix <ellixfel@gmail.com>
2024-01-17 17:23:31 -0300

    termine

--- movies/models.py
+++ movies/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 
-class Movies(models.Model):
+class Country(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=50)
-    year = models.IntegerField(blank=False, null=False)
-    director = models.CharField(max_length=50)
-    studio = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, blank=False, null=False)
 
 class Directors(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    bornYear = models.IntegerField(blank=False, null=False)
+
+class Movies(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    year = models.IntegerField(blank=False, null=False)
+    director = models.ForeignKey(Directors,on_delete=models.CASCADE)
+    country = models.ForeignKey(Country,on_delete=models.CASCADE)
+    studio = models.CharField(max_length=50)
+    
+
+
+
+
+
+    
+    
+    
 
 
     
--- movies/views.py
+++ movies/views.py
@@ -1,37 +1,36 @@
 import json
 from django.http import JsonResponse   
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from .models import Movies
 from .models import Directors
-from django.shortcuts import get_object_or_404
+from .models import Country
 
 #-----------------------------------------------------------------------------------
 #CRUD OF MOVIES MODEL
 
-# @csrf_exempt
-# def getMovies(request):
-#         allmovies=Movies.objects.all()
-#         getthatfuckinmovies=[allmovies.title for movie in allmovies]
-        
-#         return JsonResponse({getthatfuckinmovies})
+@csrf_exempt
+def getMovies(request):
+    if request.method=='GET':
+        allmovies=Movies.objects.all()
+      
+        return JsonResponse(allmovies)
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid method'}, status=405)
 
 
 @csrf_exempt  
 def getMoviesId(request, movieId):
         
     movie = get_object_or_404(Movies, id=movieId)
-        
-    movieName = movie.title
-    movieYear = movie.year
-    movieDirector = movie.director
-    movieStudio = movie.studio
-    
-
+    country = get_object_or_404(Country, id=movieId)
+    director = get_object_or_404(Directors, id=movieId)
     return JsonResponse({
-            'Movie' : movieName,
-            'Year': movieYear,
-            'Director': movieDirector,
-            'Studio': movieStudio,
+            'Movie' : movie.title,
+            'Year': movie.year,
+            'Country': country.name,
+            'Director': director.name,
+            'Studio': movie.studio,
     })
 
 @csrf_exempt
@@ -40,19 +39,25 @@ def postMovies(request):
     if request.method == 'POST':
         
         json_data = json.loads(request.body.decode('utf-8'))
-
+        
+        country_id = json_data.get('country_id')
+        country_instance = get_object_or_404(Country, id=country_id)
+        
+        director_id = json_data.get('director_id')
+        director_instance = get_object_or_404(Directors, id=director_id)
+        
         newMovie = Movies(
             title=json_data.get('title', ''),
             year=json_data.get('year', ''),
-            director=json_data.get('director', ''),
+            country=country_instance,
+            director=director_instance,
             studio=json_data.get('studio', ''),
         )
+        
         newMovie.save()
         
         return JsonResponse({'status': 'success', 'message': 'Movie Added'})
-
     else:
-        
         return JsonResponse({'status': 'error', 'message': 'Invalid method'}, status=405)
 
 @csrf_exempt
@@ -62,11 +67,9 @@ def deleteMovies(request, movieId):
         
         movie = get_object_or_404(Movies, id=movieId)
 
-        
         movie.delete()
             
         return JsonResponse({'message': f'movie number "{movieId}" deleted successfully!'})
-    
     else:
         return JsonResponse({'error': f'movie number "{movieId}" not fofound'}, status=404)
 
@@ -97,12 +100,10 @@ def getDirectorsId(request, directorId):
     if ifrequest==True:
         director = get_object_or_404(Directors, id=directorId)
         directornumber = director.id
-        directorName = director.name
-        directorYear = director.bornYear
+        directorName = director.nameD
         return JsonResponse({
             'Director number' : directornumber,
             'Name': directorName,
-            'Year of birth' : directorYear
     })
     else:
         
@@ -116,8 +117,7 @@ def postDirectors(request):
         json_data = json.loads(request.body.decode('utf-8'))
 
         newDirector = Directors(
-            name=json_data.get('Name', ''),
-            bornYear=json_data.get('bornYear', ''), 
+            nameD=json_data.get('name', ''),
         )
         newDirector.save()
         return JsonResponse({'status': 'success', 'message': 'Director Added'})
@@ -149,8 +149,7 @@ def putDirectors(request, directorId):
         json_data = json.loads(request.body.decode('utf-8'))
         director = get_object_or_404(Directors, id=directorId)
 
-        director.name = json_data.get('Name', '')
-        director.bornYear = json_data.get('bornYear', '')
+        director.nameD = json_data.get('Name', '')
         director.save()
 
         return JsonResponse({'status': 'success', 'message': 'Director Data Changed'})
@@ -158,6 +157,27 @@ def putDirectors(request, directorId):
     #     return JsonResponse({'status': 'error', 'message': 'Nuber not Found'})
     # elif request.method!='PUT':
     #     return JsonResponse({'status': 'error', 'message': 'The method is not PUT'})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid method'}, status=405)
+    #----------------------------------------------------------------------------------
+    #Countrys
+    
+@csrf_exempt
+def postCountrys(request):
+    
+    if request.method == 'POST':
+        
+        json_data = json.loads(request.body.decode('utf-8'))
 
-    # else:
-    #     return JsonResponse({'status': 'error', 'message': 'Invalid number'}, status=405)
+        newCountry = Country(
+            nameC=json_data.get('name', ''),    
+            
+        )
+        newCountry.save()
+        
+        return JsonResponse({'status': 'success', 'message': 'Country Added'})
+
+    else:
+        
+        return JsonResponse({'status': 'error', 'message': 'Invalid method'}, status=405)
+    
